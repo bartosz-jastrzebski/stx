526cc157631fa16e43f9e719e21c00ca039c3b84
Kawior <kawkag@gmail.com>
2020-08-07 16:39:37 +0200

    Created first models

--- books/models.py
+++ books/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    authors = models.ManyToManyField(Author,
+                                     related_name='books',
+                                     blank=True,
+                                     default='Not available')
+    published_date = models.CharField(max_length=10)
+    categories = models.ManyToManyField(Category,
+                                        related_name='books',
+                                        blank=True,
+                                        default='')
+    average_rating = models.PositiveSmallIntegerField()
+    ratings_count = models.PositiveIntegerField()
+    thumbnail = models.URLField()
